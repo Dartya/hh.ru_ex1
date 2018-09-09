83f60a8a991a53e74eb2811a0553648bb755305a
Alex <alexpit63@gmail.com>
2018-09-09 11:51:27 +0300

    задание выполнено путем использования считанного из строки элемента как индексатора массива значений bool - i-му элементу массива присваивается true, где i - (считанное значение - 1).

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,36 @@ public class Main {
         StringTokenizer st = new StringTokenizer(inputstring, " "); //класс, читающий данные через разделитель
         ArrayList<Integer>list = new ArrayList<>();
 
-        int currentval;     //текущая лексема в строке
-        int minval = 0;     //минимальный элемент натурального ряда в введенной строке. изначально инициализирован как 0, поэтому при первом проходе первая же лексема будет записана, как минимальная
+        int currentval;         //текущая лексема в строке
+        //int minval = 0;       //минимальный элемент натурального ряда в введенной строке. изначально инициализирован как 0, поэтому при первом проходе первая же лексема будет записана, как минимальна
+        boolean[] boolArr = new boolean[1000000000];
 
         while(st.hasMoreTokens()) {     //пока в введенной строке еще есть лексема
             currentval = Integer.parseInt(st.nextToken());
             if (currentval > 0)
-                    list.add(currentval);
+                    //list.add(currentval);
+                    boolArr[currentval-1] = true;
             else continue;
-            if (currentval > minval)
-                    minval = currentval;
-
+            /*if (currentval > minval)
+                    minval = currentval;*/
+            //boolArr[currentval-1] = true;
         }
+
+        for (int i = 0; i < boolArr.length; i++){
+            if (boolArr[i] == false) {
+                System.out.println("Минимальный отсутствующий элемент = "+(i+1));
+                break;
+            }
+            else continue;
+        }
+
     }
 }
+
+//строка для теста:
+//7 8 9 4 3 15 23 1 2
+//должэно выйти 5 - верно
+
+//строка для теста:
+//7 8 -10 -7 9 4 3 -500 15 5 23 1 2
+//должэно выйти 6 - верно
