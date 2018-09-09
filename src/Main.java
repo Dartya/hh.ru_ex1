c8627f542b0e5a8171025a455a412a00744fb4af
Alex <alexpit63@gmail.com>
2018-09-09 21:19:31 +0300

    добавил ограничение на количество данных в строке, проверку на символы. проходит все так же 20/29 тестов

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputstring = null;
+        String curval;
         try {
             inputstring = br.readLine();
             if (inputstring == null)
@@ -17,36 +18,48 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        StringTokenizer st = new StringTokenizer(inputstring, " "); //класс, читающий данные через разделитель
+        StringTokenizer st = new StringTokenizer(inputstring, " "); //объект класса, читающего данные через разделитель
         ArrayList<Integer> list = new ArrayList<>();
-        int first_elem, second_elem, currentval;
+        int first_elem, second_elem, currentval, iterator = 0;
 
         while(st.hasMoreTokens()) {
-            currentval = Integer.parseInt(st.nextToken());
-            if ((currentval > 0) && (currentval <= 1000000000 ))
+            curval = st.nextToken();
+            if (!curval.matches("\\d+")){
+                continue;}
+                else{
+                currentval = Integer.parseInt(curval);
+                if ((currentval > 0) && (currentval <= 1000000000 )){
                     list.add(currentval);
-            else continue;
-        }
-
-        Collections.sort(list);
-
-        if (list.get(0) > 1)
-            System.out.println("1");
-        else {
-            for (int i = 0; i < list.size(); i++){   //поиск минимального отсутствующего элемента введенной строки
-                first_elem = list.get(i);
-                if (i+1 < list.size())
-                    second_elem = list.get(i+1);
-                else {
-                    System.out.println("");
-                    break;
+                    iterator++;
+                    if (iterator == 1000000) break;
                 }
+                else continue;
+            }
+        }
+        if (list.size() > 0) {
+            Collections.sort(list);
 
-                if ((second_elem - first_elem) > 1) {
-                    System.out.println("" + (first_elem+1));
-                    break;
+            if (list.get(0) > 1)
+                System.out.println("1");
+            else {
+                for (int i = 0; i < list.size(); i++) {   //поиск минимального отсутствующего элемента введенной строки
+                    first_elem = list.get(i);
+                    if (i + 1 < list.size())
+                        second_elem = list.get(i + 1);
+                    else {
+                        System.out.println("");
+                        break;
+                    }
+
+                    if ((second_elem - first_elem) > 1) {
+                        System.out.println("" + (first_elem + 1));
+                        break;
+                    }
                 }
             }
+        }
+        else {
+            System.out.println("");
         }
     }
 }
@@ -69,3 +82,9 @@ public class Main {
 
 //-9 -8 -7 7 8 -10 -7 9 4 3 -500 6 7 8 15 5 23 2 1000000000
 //должно выйти 1 - верно
+
+// 1 2 d 3 8f 6 6 4 fg 7
+//должно выйти 5 - верно
+
+//blabla
+//
